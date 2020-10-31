b74fe2371d441577b61d30095e5bff87e9700951
Manuel Reithuber <git@manuel.reithuber.net>
2020-10-31 01:30:53 +0100

    statuserror: added further tests

--- statuserror/errors_test.go
+++ statuserror/errors_test.go
@@ -1,6 +1,7 @@
 package statuserror
 
 import (
+	"errors"
 	"net/http"
 	"testing"
 
@@ -12,4 +13,42 @@ func Test(t *testing.T) {
 
 	assert.Equal(t, http.StatusNotFound, err.Code())
 	assert.Equal(t, "hello", err.Error())
+	assert.Nil(t, err.Data())
+
+	var err2 = err.WithField("hello", "world")
+
+	assert.Nil(t, err.Data())
+	assert.Equal(t, map[string]interface{}{
+		"hello": "world",
+	}, err2.Data())
+
+	// err2 will be altered here - figure out if that's what we want or not
+	var err3 = err2.WithField("answer", 42)
+	assert.Equal(t, map[string]interface{}{
+		"hello":  "world",
+		"answer": 42,
+	}, err2.Data())
+
+	assert.Equal(t, err2, err3)
+}
+
+func TestUnwrap(t *testing.T) {
+	var err = Errorf(http.StatusForbidden, "reason: %w", errors.New("lol"))
+	assert.Equal(t, "reason: lol", err.Error())
+	assert.Equal(t, http.StatusForbidden, err.Code())
+	assert.Equal(t, errors.New("lol"), err.Unwrap())
+	assert.Equal(t, errors.New("lol"), errors.Unwrap(err))
+
+	var err2 = FromError(err.(error))
+	assert.Equal(t, "reason: lol", err2.Error())
+	assert.Equal(t, http.StatusForbidden, err2.Code())
+	assert.Equal(t, errors.New("lol"), err2.Unwrap())
+	assert.Equal(t, errors.New("lol"), errors.Unwrap(err2))
+
+	var err3 = From(http.StatusUnauthorized, err.(error))
+	assert.Equal(t, "reason: lol", err3.Error())
+	assert.Equal(t, http.StatusUnauthorized, err3.Code())
+	assert.Equal(t, errors.New("lol"), err3.Unwrap())
+	assert.Equal(t, errors.New("lol"), errors.Unwrap(err3))
+
 }
